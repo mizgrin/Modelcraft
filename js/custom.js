e84b901b9f15990cd893f6325f139a9167059959
mijan123 <msmizans@gmail.com>
2019-11-19 14:02:08 +0545

    team members

--- js/custom.js
+++ js/custom.js
@@ -51,6 +51,33 @@ jQuery(document).ready(function( $ ){
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
 });
+ var $teamwrapper = $('.modelcraft-team-slider').isotope({
+  itemSelector: '.modelcraft-team-single',
+  layoutMode: 'fitRows',
+   filter: '.dance'
+});
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+
+  // show if name ends with -ium
+
+};
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $teamwrapper.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
 
  
   $(".model-craft-slider").slick({
